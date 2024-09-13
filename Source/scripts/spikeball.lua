448333b2980e26ca35b563bfb36d66cff690af52
Richard Mitchell <richard.a.mitchell@gmail.com>
2024-09-13 06:44:35 -0500

    Started spikeball implementation

--- Source/scripts/spikeball.lua
+++ Source/scripts/spikeball.lua
@@ -1,1 +1,16 @@
--- TODO IMPLEMENT
+local gfx <const> = playdate.graphics
+
+local spikeballImage <const> = gfx.image.new("images/spikeball")
+
+class("Spikeball").extends(gfx.sprite)
+
+function spikeBall:init(x, y, entity)
+    self:setZIndex(Z_INDEXES.Hazard)
+    self:setImage(spikeballImage)
+    self:setCenter(0, 0)
+    self:moveTo(x, y)
+    self:add()
+
+    self:setTag(TAGS.Hazard)
+    self:setCollideRect(4, 4, 8, 8)
+end
